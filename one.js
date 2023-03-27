490f4bdab20bfb4ca91a91c7e82b47f9bc8bc0ad
HelenKantani <129065421+HelenKantani@users.noreply.github.com>
2023-03-27 19:09:14 +0300

    Add files via upload

---
+++ one.js
@@ -0,0 +1,8 @@
+let FirstNumber = Number(prompt('First number'));
+let SecondNumber = Number(prompt('Second number'));
+let ThirdNumber = Number(prompt('Third number'));
+
+console.log(FirstNumber + SecondNumber + ThirdNumber);
+console.log(FirstNumber - SecondNumber- ThirdNumber);
+console.log(FirstNumber/SecondNumber/ThirdNumber);
+console.log(FirstNumber*SecondNumber*ThirdNumber);
---
+++ three.js
@@ -0,0 +1,4 @@
+let priceNumber = 12999;
+let SalePriceNumber = prompt('Discount price');
+
+console.log (priceNumber-(priceNumber/100)*(SalePriceNumber));
